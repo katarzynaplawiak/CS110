6dd6bb6395636a8877bff8dc152cf7947788f538
Kasia <kjplaw12@smumn.edu>
2013-01-15 19:34:16 -0600

    changed the code

--- CS110/src/assignements/HelloWorld.java
+++ CS110/src/assignements/HelloWorld.java
@@ -3,6 +3,10 @@ package assignements;
 public class HelloWorld {
 	public static void main(String[] arg) {
 		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("A new line");
 	}
 
 }
