c6616079f3b8d6dd4e3537550ceddca79e87e90f
Kasia <kjplaw12@smumn.edu>
2013-02-12 19:41:20 -0600

    Pe417

--- CS110/src/assignment/chap4/Pe417.java
+++ CS110/src/assignment/chap4/Pe417.java
@@ -3,25 +3,31 @@ package assignment.chap4;
 import java.util.Scanner;
 
 public class Pe417 {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enter the number of lines: ");
-		int numberOfLines = input.nextInt();
-		for (int i = 1; i <=numberOfLines ; i++){
-			for (int j =1; j <=(numberOfLines-i); j++){
-				System.out.print("  ");
-			}
-			for ( int j = i; j>=1; j--){
-				System.out.print(j+ " ");
-			}
-			for ( int j = 2; j <= i; j++){
-				System.out.print(j + " ");
-			}
-			System.out.println();
-		}
-	}	
+Scanner input = new Scanner(System.in);
+System.out.println("Enter the number of lines: ");
+int numberOfLines = input.nextInt();
+for (int i = 1; i <=numberOfLines ; i++){
+for (int j =1; j <=(numberOfLines-i); j++){
+	System.out.print("   ");
+	}
+for ( int k = i; k>=1; k--){
+	if(k >= 10){
+		System.out.print(" "+k);
+	}
+	else{
+		System.out.print("  "+k);
+	}
+
+	}
+for ( int m = 2; m <= i; m++){
+	if (m>=10){
+		System.out.print(" "+m);
+	}
+	else{
+System.out.print("  "+m);
+}
+}
+System.out.println();}	
+}
 }
