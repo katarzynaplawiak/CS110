dfe9cfea34bd56495557128ce730fa5e53346e16
Kasia <kjplaw12@smumn.edu>
2013-02-20 23:48:29 -0600

    improved Pe419 (more general)

--- CS110/src/assignment/chap4/Pe419.java
+++ CS110/src/assignment/chap4/Pe419.java
@@ -2,29 +2,35 @@ package assignment.chap4;
 
 public class Pe419 {
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
 		final int NUMBER_OF_LINES = 8;
-		String space = "    ";
+		int max = (int)Math.pow(2, NUMBER_OF_LINES);
+		String s = String.valueOf(max);
+		int fieldSize = s.length() +1;
+	
 		for (int i = 1; i <= NUMBER_OF_LINES ; i++){
 			for (int j =1; j <= (NUMBER_OF_LINES-i); j++){
-				System.out.print(space);
+				printSpace(fieldSize);
 				}
 			for ( int k =1 ; k<=i; k++){
 					int power = k-1;
-				System.out.printf("%4d", (int)Math.pow(2, power));
+				System.out.printf("%"+fieldSize+"d", (int)Math.pow(2, power));
 				power++;}
 				
 			for ( int m=2; m <= i; m++){
 			int power = i-m;
-			System.out.printf("%4d",(int)Math.pow(2, power));
+			System.out.printf("%"+fieldSize+"d",(int)Math.pow(2, power));
 			power--;			
 			}
 			System.out.println();
 			}
 			
 			}	
+	public static void printSpace (int size){
+		for (int i = 0; i < size; i++){
+			System.out.print(" ");
+	}
 			}
+}
 			
