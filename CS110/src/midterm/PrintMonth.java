8fd690bcc9349115b13a7efe639c5e76f3dd6e1a
Kasia <kjplaw12@smumn.edu>
2013-02-28 09:12:32 -0600

    added PrintMonth

--- CS110/src/midterm/PrintMonth.java
+++ CS110/src/midterm/PrintMonth.java
@@ -7,35 +7,26 @@ public class PrintMonth {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		printMonth (5, 31);
+		printMonth(5, 31);
 		printMonth(2, 28);
 		printMonth(6, 30);
 
 	}
-	public static void printMonth (int startDay, int dayInMonth){
-		String [] days = {"Sun","Mon", "Tue", "Wed", "Thr", "Fri", "Sat"};
-		
-		
-		for (int i =0; i< days.length; i++){
-			System.out.print(days[i]+ " ");
-		}
-		System.out.println();
-	
-		for (int j = 0; j< 35; j++){
-			int count = 0;
-			for (int k =0; k<(35-dayInMonth);k++ ){
-				System.out.printf("%4s", " ");
-				count++;
-			
-			
-			for (int i = 1; i<=dayInMonth; i++){
+
+	public static void printMonth(int startDay, int dayInMonth) {
+		System.out.println(" Sun Mon Tue Wed Thr Fri Sat");
+
+		for (int i = 0; i < startDay; i++)
+			System.out.print("    ");
+
+		for (int i = 1; i <= dayInMonth; i++) {
 			System.out.printf("%4d", i);
+
+			if ((i + startDay) % 7 == 0)
+				System.out.println();
 		}
-			System.out.println();
-			
-			}
-		}
+
+		System.out.println();
+
 	}
 }
-
-
