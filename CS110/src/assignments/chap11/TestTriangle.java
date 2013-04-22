ecf6c762326b8720f7805dd173f5c6a5596c805b
Kasia <kjplaw12@smumn.edu>
2013-04-22 14:05:13 -0500

    testTriangle correction

--- CS110/src/assignments/chap11/TestTriangle.java
+++ CS110/src/assignments/chap11/TestTriangle.java
@@ -5,10 +5,23 @@ import java.util.Scanner;
 public class TestTriangle {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Enter three sides of a triangle: ");
-		double side1 = input.nextDouble();
-		double side2 = input.nextDouble();
-		double side3 = input.nextDouble();
+		double side1;
+		double side2;
+		double side3;
+		while (true) {
+			System.out.print("Enter three sides of a triangle: ");
+			side1 = input.nextDouble();
+			side2 = input.nextDouble();
+			side3 = input.nextDouble();
+			if (side1 > side2 + side3 || side2 > side1 + side3
+					|| side3 > side1 + side2) {
+				System.out
+						.println("Triangle is impossible to construct (two sides add to less than the third)");
+			} else {
+				break;
+			}
+		}
+
 		System.out.print("Enter the color of a triangle : ");
 		String color = input.next();
 		System.out.print("Is the trianle filled (true) or empty (false) ? ");
