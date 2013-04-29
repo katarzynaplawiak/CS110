49cfe79144796d042faf79b4a80a1f2cbf34f394
Kasia <kjplaw12@smumn.edu>
2013-04-29 15:09:05 -0500

    displayWordsearch

--- CS110/src/assignments/chap12/TestDisplayWordSearch.java
+++ CS110/src/assignments/chap12/TestDisplayWordSearch.java
@@ -8,11 +8,11 @@ public class TestDisplayWordSearch {
 	public static void main(String[] args) {
 		JFrame frame = new DisplayWordSearch();
 		frame.setLayout(new GridLayout(4, 1, 5, 5));
-		frame.setSize(600, 600);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
 		frame.pack();
+		frame.setVisible(true);
+		
 
 	}
 }
